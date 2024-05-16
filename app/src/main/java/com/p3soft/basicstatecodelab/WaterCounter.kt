49858d99cbadf4057fdce81ec258324ce37ffeac
Jack Sage-Phoenix <jack@jackmeyers.co>
2024-05-16 13:09:27 -0500

    6. State driven UI

--- app/src/main/java/com/p3soft/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/p3soft/basicstatecodelab/WaterCounter.kt
@@ -17,12 +17,17 @@ import androidx.compose.ui.unit.dp
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
-        Text("You've had $count glasses.")
+        if(count > 0) {
+            // This text is "present" if the button has been pressed at least once
+            // Elsewise it is "absent"
+            Text("You've had $count glasses.")
+        }
         Button(
             onClick = {
                 count++
             },
-            Modifier.padding(top = 8.dp)
+            Modifier.padding(top = 8.dp),
+            enabled = count < 10,
         ) {
             Text("Add one")
         }
