5aaf2ea75770faf1b3207d6cf52544317b5888bc
Jack Sage-Phoenix <jack@jackmeyers.co>
2024-05-16 13:44:31 -0500

    9. State hoisting

--- app/src/main/java/com/p3soft/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/p3soft/basicstatecodelab/WaterCounter.kt
@@ -34,3 +34,66 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     }
 
 }
+
+@Composable
+fun StatelessCounter(
+    count: Int,
+    onIncrement: () -> Unit,        // accept a callback function to update state
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if(count > 0) {
+            Text("You've had ${count} glasses.")
+        }
+        Button(
+            onClick = onIncrement,
+            Modifier.padding(top = 8.dp),
+            enabled = count < 10,
+        ) {
+            Text("Add one")
+        }
+    }
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) } // State lives here
+    StatelessCounter(       // The component that actually displays
+        count,              // the current count
+        { count++ },        // the (anonymous) callback function
+        modifier
+    )
+}
+
+/* NOTES
+
+- Your stateless composables can now be reused:
+
+@Composable
+fun StatefulCounter() {
+    var waterCount by remember { mutableStateOf(0) }
+
+    var juiceCount by remember { mutableStateOf(0) }
+
+    StatelessCounter(waterCount, { waterCount++ })
+    StatelessCounter(juiceCount, { juiceCount++ })
+}
+
+If juiceCount is modified then StatefulCounter is recomposed.  During recomposition, Compose identifies
+which functions read juiceCount and triggers recomposition of *only* those functions.
+
+- Your stateful Composable can now provide the same state to multiple Composables
+
+@Composable
+fun StatefulCounter() {
+   var count by remember { mutableStateOf(0) }
+
+   StatelessCounter(count, { count++ })
+   AnotherStatelessMethod(count, { count *= 2 })
+}
+
+If the count is updated by either StatelessCounter or AnotherStatelessMethod, everything is recomposed,
+which is as expected.
+
+
+ */
